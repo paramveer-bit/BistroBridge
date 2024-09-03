341ad1933c660bddf960b88a65750aebb927ccc7
Paramveer-bit <67367312+paramveer-bit@users.noreply.github.com>
2024-09-03 12:48:46 +0530

    chore: Update layout, add Card component, and create Customer model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
 import Head from 'next/head';
 import SignIn from '@/components/SignIn';
+import Pic1 from '@/assets/PIc1.png'
+import Image from 'next/image';
+
+const Card = ({pic,heading,data}:{data:String,heading:String,pic:String}) =>{
+    return (
+        <div className='bg-pink-400 p-2 flex flex-col items-center'>
+            <Image src={Pic1} alt=""/>
+            <h1 className='text-2xl font-semibold px-3 text-center'>
+                Uninterrupted billing operations
+            </h1>
+            <p className='px-2 text-center font-medium'>
+                Experience smooth checkout with tablet, mobile, and desktop-based Touch POS systems that ensure smooth checkout, even offline.
+            </p>
+        </div>
+    )
+}
+
+
 const ExamplePage = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-          <SignIn />
-            <Head>
-                <title>Example Page</title>
-                <meta name="description" content="This is an example page" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+        <div className="min-h-screen bg-gray-100">
+            {/* Nav */}
+          <div className='flex w-full justify-between bg-black text-white px-4 py-4 pl-10 font-serif font-bold pt-5'>
+            <h1 className='text-4xl'>BaristoBridge</h1>
+            <div className='my-auto bg-red-500 px-3 py-1 mr-8 text-xl font-bold rounded-lg'><SignIn/></div>
+          </div>
 
-            <main className="text-center">
-                <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to the Example Page!</h1>
-                <p className="text-lg text-gray-700">This is a simple example of a page created with TypeScript and Tailwind CSS.</p>
-            </main>
 
-            <div className="absolute bottom-0 w-full text-center p-4 bg-gray-200">
-                <p>Powered by Next.js</p>
+            {/* Hero One */}
+
+          <div className='flex w-full flex-col pt-10 bg-stone-300 min-h-screen px-20'>
+            <h1 className='text-center text-5xl font-sans font-bold'>Why choose Baristo Bridge ?</h1>
+            <div className='grid grid-cols-4 gap-4 pt-16'>
+                <Card da/>
+                {/* <Card />
+                <Card />
+                <Card />
+                <Card /> */}
             </div>
+
+          </div>
+            
+        
+        
+
+
         </div>
     );
 };
